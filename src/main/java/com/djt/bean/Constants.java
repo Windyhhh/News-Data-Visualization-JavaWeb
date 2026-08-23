4dccff8b71418a6fb9a9289eaef027bed9aa6e5a
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 17:49:02 +0800

    Add src/main/java/com/djt/bean/Constants.java

---
+++ src/main/java/com/djt/bean/Constants.java
@@ -0,0 +1,10 @@
+package com.djt.bean;
+
+public class Constants {
+
+	public static final String JDBC_DRIVER = "jdbc.driver";
+	public static final String JDBC_DATASOURCE_SIZE = "jdbc.datasource.size";
+	public static final String JDBC_URL = "jdbc.url";
+	public static final String JDBC_USER = "jdbc.user";
+	public static final String JDBC_PASSWORD = "jdbc.password";
+}
