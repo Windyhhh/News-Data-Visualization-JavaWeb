efa6326a2e80229af225ea1812562bdc4872b18a
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 17:49:06 +0800

    Add src/main/java/com/djt/servlet/TestServlet.java

---
+++ src/main/java/com/djt/servlet/TestServlet.java
@@ -0,0 +1,24 @@
+package com.djt.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class TestServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doPost(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=utf-8");
+        PrintWriter pw = response.getWriter();
+        pw.write("My Servlet");
+        pw.flush();
+        pw.close();
+    }
+}
